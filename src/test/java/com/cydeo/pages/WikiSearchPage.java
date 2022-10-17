23094038e012ddd7d62180e0a080f6f0b1d2864c
BayramBilal <bilalbayramb22@gmail.com>
2022-10-17 10:27:17 +0300

    tag is added

--- src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -1,22 +1,20 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class WikiSearchPage {
 
-    public WikiSearchPage () throws StaleElementReferenceException {
-
+    public WikiSearchPage(){
         PageFactory.initElements(Driver.getDriver(), this);
-
     }
-    @FindBy(id ="searchInput")
+
+    @FindBy(id = "searchInput")
     public WebElement searchBox;
 
-    @FindBy(className= "sprite svg-search-icon")
+    @FindBy(xpath = "//button[@type='submit']")
     public WebElement searchButton;
 
     @FindBy(id = "firstHeading")
--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,11 +7,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+
+        },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@wip",
+        publish = false
+
 
 )
 public class CukesRunner {
